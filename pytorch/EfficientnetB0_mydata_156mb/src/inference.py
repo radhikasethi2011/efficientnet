542b4a18c22958d13df94156fef7c28f3cde313b
Radhika <radhika.sethi0312@gmail.com>
2022-05-22 08:08:28 +0530

    Update inference.py

--- pytorch/EfficientnetB0_mydata_156mb/src/inference.py
+++ pytorch/EfficientnetB0_mydata_156mb/src/inference.py
@@ -13,43 +13,50 @@ IMAGE_SIZE = 224
 DEVICE = 'cpu'
 # Class names.
 
-dataset_train, dataset_test, dataset_valid, dataset_classes = get_datasets()
-print(f"[INFO]: Number of testing images: {len(dataset_test)}")
-print(f"[INFO]: Class names: {dataset_classes}\n")
-# Load the training and validation data loaders.
-train_loader, test_loader, valid_loader = get_data_loaders(dataset_train, dataset_test, dataset_valid)
-dataiter = iter(test_loader)
-images, labels = dataiter.next()
-print("##############################")
-print("image shape ",images.shape)
-print("label shape ", labels.shape)
-# Load the trained model.
 model = build_model(pretrained=False, fine_tune=False, num_classes=4)
 checkpoint = torch.load('/content/efficientnet/pytorch/EfficientnetB0_mydata_156mb/outputs/model.pth', map_location=DEVICE)
 print('Loading trained model weights...')
 model.load_state_dict(checkpoint['model_state_dict'])
 
-# Get all the test image paths.
-correct_count, all_count = 0, 0
-for images,labels in test_loader:
-  for i in range(len(labels)):
+twod_path_list = []
+class_names = ['Te-gl', 'Te-me', 'Te-no', 'Te-pi']
+class_dict = {'Te-gl': 'glioma', 'Te-me': 'meningioma', 'Te-no': 'notumor', 'Te-pi' : 'pituitary'}
+for count, i in enumerate(class_dict): 
+  all_image_paths = glob.glob(f'/content/Testing/{class_dict[i]}/*')
+  twod_path_list.append(all_image_paths)
   
-    images, labels = images.to(DEVICE), labels.to(DEVICE)
-    img = images[i].view(1,3, 224, 224)
-    #img = torch.unsqueeze()
-    with torch.no_grad():
-        logps = model(img)
+all_image_path_list = [item for sublist in twod_path_list for item in sublist]
 
-    
-    ps = torch.exp(logps)
-    probab = list(ps.cpu()[0])
-    pred_label = probab.index(max(probab))
-    true_label = labels.cpu()[i]
-    print("true label: ", true_label)
-    print("pred label: ", pred_label)
-    if(true_label == pred_label):
-      correct_count += 1
-    all_count += 1
+k = 0
+ctr = 0
+for image_path in all_image_path_list: 
+  gt_class_name = class_dict[image_path.split(os.path.sep)[-1][:5]]
+  image = cv2.imread(image_path)
+  orig_image = image.copy()
+  # Preprocess the image
+  image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+  transform = transforms.Compose([
+      transforms.ToPILImage(),
+      transforms.Resize((IMAGE_SIZE, IMAGE_SIZE)),
+      transforms.ToTensor(),
+      transforms.Normalize(
+          mean=[0.485, 0.456, 0.406],
+          std=[0.229, 0.224, 0.225]
+      )
+  ])
+  image = transform(image)
+  image = torch.unsqueeze(image, 0)
+  image = image.to(DEVICE)
+  
+  # Forward pass throught the image.
+  outputs = model(image)
+  outputs = outputs.detach().numpy()
+  pred_class_name = class_dict[class_names[np.argmax(outputs[0])]]
+  print(f'K = {k}, GT = {gt_class_name}, Pred = {pred_class_name}')
+  k+=1
+  if(gt_class_name==pred_class_name): 
+    ctr+=1
+print("CTR: ", ctr)
+print('acc: ', ctr/1311)
 
-print("Number Of Images Tested =", all_count)
-print("\nModel Accuracy =", (correct_count/all_count))
+
